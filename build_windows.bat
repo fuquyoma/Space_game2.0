d3c91dd8d7e6496393bb8852f9a2848dc29a460b
Artyom <89246164+fuquyoma@users.noreply.github.com>
2024-12-17 19:40:34 +0300

    Delete build_windows.bat

--- build_windows.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-pyinstaller --onefile --add-data "resources;resources" --add-data "alien.py;." --add-data "bonus.py;." --add-data "bullet.py;." --add-data "save_load.py;." --add-data "settings.py;." --add-data "ship.py;." alien_invasion.py
